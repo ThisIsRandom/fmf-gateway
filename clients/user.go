aff06b84055ed912e3cc289a35c59dd95197fc8f
Your Name <you@example.com>
2023-02-05 23:14:52 +0100

    .

--- clients/user.go
+++ clients/user.go
@@ -2,6 +2,8 @@ package clients
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
 	"log"
 
 	"github.com/thisisrandom/user-service/proto"
@@ -13,8 +15,27 @@ type client struct {
 	Conn proto.TokenClient
 }
 
+type BasicAuth struct {
+	Username string
+	Password string
+}
+
+func (b BasicAuth) GetRequestMetaData(ctx context.Context, in ...string) (map[string]string, error) {
+	auth := fmt.Sprintf("%s:%s", b.Username, b.Password)
+	encrypted := base64.StdEncoding.EncodeToString([]byte(auth))
+	return map[string]string{
+		"authorization": "Basic " + encrypted,
+	}, nil
+}
+
+
 func InitTokenClient(url string) (*client, error) {
 	log.Println(url)
+
+	creds := grpc.WithTransportCredentials(
+		insecure.NewCredentials(),
+		grpc.WithPerRPCCredentials()
+	)
 
 	cc, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
